160e98fd86c2e0128a6fe0f7f711d64cd6255864
nov <nov@mail.ru>
2017-05-03 11:25:30 +0400

    Добавление проекции

--- src/main/java/ru/javabegin/training/springlibrary/dao/impl/BookDAOImpl.java
+++ src/main/java/ru/javabegin/training/springlibrary/dao/impl/BookDAOImpl.java
@@ -2,6 +2,11 @@ package ru.javabegin.training.springlibrary.dao.impl;
 
 import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.ProjectionList;
+import org.hibernate.criterion.Projections;
+import org.hibernate.transform.Transformers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,16 +23,30 @@ public class BookDAOImpl implements BookDAO {
     @Autowired
     private SessionFactory sessionFactory;
 
-    private List<Book> books;
+    private ProjectionList bookProjection;
+
+    public BookDAOImpl() {
+        bookProjection = Projections.projectionList();
+        bookProjection.add(Projections.property("id"), "id");
+        bookProjection.add(Projections.property("name"), "name");
+        bookProjection.add(Projections.property("image"), "image");
+        bookProjection.add(Projections.property("genre"), "genre");
+        bookProjection.add(Projections.property("pageCount"), "pageCount");
+        bookProjection.add(Projections.property("isbn"), "isbn");
+        bookProjection.add(Projections.property("publisher"), "publisher");
+        bookProjection.add(Projections.property("author"), "author");
+        bookProjection.add(Projections.property("publishYear"), "publishYear");
+        bookProjection.add(Projections.property("descr"), "descr");
+    }
 
 
     @Transactional
     @Override
     public List<Book> getBooks() {
+        DetachedCriteria bookListCriteria = DetachedCriteria.forClass(Book.class, "b");
+        createAliases(bookListCriteria);
 
-        books = (List<Book>) sessionFactory.getCurrentSession()
-                .createCriteria(Book.class)
-                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
+        List<Book> books = createBookList(bookListCriteria);
 
         return books;
     }
@@ -50,6 +69,21 @@ public class BookDAOImpl implements BookDAO {
     @Override
     public List<Book> getBooks(Character letter) {
         return null;
+    }
+
+    private void createAliases(DetachedCriteria criteria) {
+        criteria.createAlias("b.author", "author");
+        criteria.createAlias("b.genre", "genre");
+        criteria.createAlias("b.publisher", "publisher");
+    }
+
+
+    private List<Book> createBookList(DetachedCriteria bookListCriteria) {
+        Criteria criteria = bookListCriteria.getExecutableCriteria
+                (sessionFactory.getCurrentSession());
+        criteria.addOrder(Order.asc("b.name")).setProjection(bookProjection).
+                setResultTransformer(Transformers.aliasToBean(Book.class));
+        return criteria.list();
     }
 
 }
